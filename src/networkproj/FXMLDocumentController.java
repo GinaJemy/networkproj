74fbee288900d3a95f712639f9eb313be7a57fe3
mohamed maged <mohamedmaged35@yahoo.com>
2017-12-19 15:36:36 +0200

    add load function

--- src/networkproj/FXMLDocumentController.java
+++ src/networkproj/FXMLDocumentController.java
@@ -37,6 +37,7 @@ public class FXMLDocumentController implements Initializable {
        @FXML public Button start;
        @FXML public Button save;
        @FXML public TextArea hex;
+       @FXML public Button load;
 EventHandler<ActionEvent> action = changeTabPlacement();
 SniffingThread s =new SniffingThread();
 public static String protocoll[] = {"HOPOPT","ICMP","IGMP","GGP","IPv4","ST","TCP","CBT","EGP","IGP","BBN-RCC-MON","NVP-II","PUP","ARGUS (deprecated)","EMCON","XNET","CHAOS","UDP","MUX","DCN-MEAS","HMP","PRM","XNS-IDP","TRUNK-1","TRUNK-2","LEAF-1","LEAF-2","RDP","IRTP","ISO-TP4","NETBLT","MFE-NSP","MERIT-INP","DCCP","3PC","IDPR","XTP","DDP","IDPR-CMTP","TP++","IL","IPv6","SDRP","IPv6-Route","IPv6-Frag","IDRP","RSVP","GRE","DSR","BNA","ESP","AH","I-NLSP","SWIPE (deprecated)","NARP","MOBILE","TLSP","SKIP","IPv6-ICMP","IPv6-NoNxt","IPv6-Opts","CFTP","SAT-EXPAK","KRYPTOLAN","RVD","IPPC","SAT-MON","VISA","IPCV","CPNX","CPHB","WSN","PVP","BR-SAT-MON","SUN-ND","WB-MON","WB-EXPAK","ISO-IP","VMTP","SECURE-VMTP","VINES","TTP","IPTM","NSFNET-IGP","DGP","TCF","EIGRP","OSPFIGP","Sprite-RPC","LARP","MTP","AX.25","IPIP","MICP (deprecated)","SCC-SP","ETHERIP","ENCAP","GMTP","IFMP","PNNI","PIM","ARIS","SCPS","QNX","A/N","IPComp","SNP","Compaq-Peer","IPX-in-IP","VRRP","PGM","0-hopprotocol","L2TP","DDX","IATP","STP","SRP","UTI","SMP","SM (deprecated)","PTP","ISIS over IPv4","FIRE","CRTP","CRUDP","SSCOPMCE","IPLT","SPS","PIPE","SCTP","FC","RSVP-E2E-IGNORE","Mobility Header","UDPLite","MPLS-in-IP","manet","HIP","Shim6","WESP","ROHC"};
@@ -51,6 +52,7 @@ else{
     start.setDisable(true);
      s.CapturePackets();
      save.setDisable(true);
+     load.setDisable(true);
 }
  
  }
@@ -64,6 +66,8 @@ else{
          s.stop();
          start.setDisable(false);
          save.setDisable(false);
+         load.setDisable(false);
+
          }
          
      }
@@ -126,5 +130,16 @@ else{
                              j.writePacket(pac.get(i));
                          }
 
+     }
+     public void loadpac() throws IOException
+     {
+         packets.clear();
+         JpcapCaptor captor=JpcapCaptor.openFile("test.pcap");
+         while(true)
+         {
+             Packet packet=captor.getPacket();
+             if(packet==null)break;
+                 packets.add((IPPacket)packet);
+         }
      }
 }
